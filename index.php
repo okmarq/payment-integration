c53b29f4ab6811c9441ae23da6a631e9bf2138ac
Joel Okoromi <okmarq@gmail.com>
2021-11-07 15:21:53 +0100

    transaction verification working

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <title>Payment Integration</title>
 </head>
@@ -39,9 +39,9 @@
         </form>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="/js/jquery-3.6.0.min.js"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://js.paystack.co/v1/inline.js"></script>
     <script>
@@ -58,14 +58,13 @@
                     alert('Window closed.');
                 },
                 callback: function(response) {
-                    let message = 'Payment complete! Reference: ' + response.reference;
-                    alert(message);
-                    window.location = "http://payment-integration.test/verify_transaction.php?reference=" + response.reference;
+                    // window.location = "http://www.yoururl.com/verify_transaction.php?reference=" + response.reference;
                     $.ajax({
-                        url: 'verify_transaction.php',
+                        url: 'http://payment-integration.test/verify_transaction.php?reference=' + response.reference,
                         method: 'get',
                         success: function(response) {
                             console.log(response);
+                            // the details needed will be extracted from here and used
                         }
                     });
                 }
@@ -76,28 +75,3 @@
 </body>
 
 </html>
-
-{
-"reference": "T266609159585221",
-"trans": "1435631706",
-"status": "success",
-"message": "Approved",
-"transaction": "1435631706",
-"trxref": "T266609159585221"
-}
-{
-"reference": "T266609159585221",
-"trans": "1435631706",
-"status": "success",
-"message": "Approved",
-"transaction": "1435631706",
-"trxref": "T266609159585221"
-}
-{
-"reference": "T154882024429226",
-"trans": "1435637063",
-"status": "success",
-"message": "Approved",
-"transaction": "1435637063",
-"trxref": "T154882024429226"
-}
--- verify_transaction.php
+++ verify_transaction.php
@@ -1,28 +1,34 @@
 <?php
-include 'env_util';
+include 'env_util.php';
 
-  $curl = curl_init();
-  
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://api.paystack.co/transaction/verify/:reference",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-      "Authorization: Bearer getenv('Test_Secret_Key')",
-      "Cache-Control: no-cache",
-    ),
-  ));
-  
-  $response = curl_exec($curl);
-  $err = curl_error($curl);
-  curl_close($curl);
-  
-  if ($err) {
-    echo "cURL Error #:" . $err;
-  } else {
-    echo $response;
-  }
+$reference = $_GET['reference'];
+
+if ($reference == '') {
+  header('Location: javascript://history.go(-1)');
+}
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.paystack.co/transaction/verify/" . rawurlencode($reference),
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer " . getenv('Test_Secret_Key'),
+    "Cache-Control: no-cache",
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
